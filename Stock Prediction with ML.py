0962d16a9ef26dc13052ffe70f3806058119c647
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2020-06-05 22:34:06 -0400

    Update Stock Prediction with ML.py

--- Stock Prediction with ML.py
+++ Stock Prediction with ML.py
@@ -8,17 +8,16 @@ from keras.layers import Dense, LSTM
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
-from pandas.tseries import converter
+from pandas.plotting import register_matplotlib_converters
 import pandas as pd
 import numpy as np
 import pandas_datareader as web
 import math
-
-converter.register()
+register_matplotlib_converters()
 
 # static variables
-stock_symbol = 'JWCA-H.V'
-start_date = '2020-04-03'
+stock_symbol = 'HOU.TO'
+start_date = '2008-01-15'
 
 # Import Libraries
 plt.style.use('fivethirtyeight')
@@ -54,6 +53,9 @@ training_data_len = math.ceil(len(dataset) * .8)
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(dataset)
 
+# Print Scaled Data
+# print(scaled_data)
+
 # Create the training data set
 # Create the scaled training data set
 train_data = scaled_data[0:training_data_len, :]
@@ -64,8 +66,68 @@ y_train = []
 
 for i in range(60, len(train_data)):
     x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i: 0])
+    y_train.append(train_data[i, 0])
     if i <= 60:
         print(x_train)
         print(y_train)
         print()
+    elif not len(train_data) >= 60:
+        print('Not enough data')
+        print()
+
+# Conevert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshapes the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+print(x_train.shape)
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+# Create the testing dataset
+# Create a new array containing scaled values
+test_data = scaled_data[training_data_len-60:, :]
+# Create the datasets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+# Convert the daata to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# Get the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(predictions-y_test)**2)
+print(rmse)
+
+# Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+# Visualize the data
+plt.figure(figsize=(16, 8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc="lower right")
+print(plt.show())
