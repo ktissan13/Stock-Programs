6fa348c5858fc1e1ed4b2bed26da2923d64a3fb3
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2020-06-05 21:05:44 -0400

    Create Stock Predictor 2.0.py

---
+++ Stock Predictor 2.0.py
@@ -0,0 +1,15 @@
+# Stock Predictor
+# Version: 2
+# Tissan Kugathas
+# Start Date: June 5th 2020
+# End Date: TBA
+
+# IMPORT AND INSTALL DEPENDENCIES
+# Installing Feature will be added soon in order for other users to use without any problems occuring
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
